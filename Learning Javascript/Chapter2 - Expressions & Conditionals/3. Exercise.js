0f8552aa89182795987ca00d68bfb0f781b5f80d
Priyam Dey <deypriyam807@gmail.com>
2025-05-24 05:42:43 +0000

    Practice Set

---
+++ Learning Javascript/Chapter2 - Expressions & Conditionals/3. Exercise.js
@@ -0,0 +1,9 @@
+// Q1- Use logical operators to find whether the age of a person lies between 10 and 20?
+
+// Q2- Demonstrate the use of switch case statements in javascript
+
+// Q3- write a program in javscript to find whether a number is divisible by 2 and 3
+
+// Q4- write a program in javascript to find whether a number is divisible by either 2 or 3
+
+// Q5- print "you can drive" or "you cannot drive" based on age being greater than 18 using ternary operator

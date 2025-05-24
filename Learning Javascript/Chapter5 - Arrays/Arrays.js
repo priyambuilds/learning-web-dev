22a5fda4b8d380ea1179490d1618c6847a5b8ed0
Priyam Dey <deypriyam807@gmail.com>
2025-05-24 09:44:26 +0000

    Arrays in JS

---
+++ Learning Javascript/Chapter5 - Arrays/Arrays.js
@@ -0,0 +1,5 @@
+let a = [1 ,2 ,3 ,4 ,5, null, undefined, true, false, "hello", "world", [1, 2, 3], {name: "Harry", age: 20}, function(){console.log("Hello")}, Symbol("Hello"), BigInt(1234567890123456789012345678901234567890)]
+console.log(a)
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])

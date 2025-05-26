f6faa457ae25f19198c36606acd5552c8b863bcf
Priyam Dey <deypriyam807@gmail.com>
2025-05-26 14:46:25 +0000

    Initialize script with item management functions for Persistent ToDo List

--- Learning Javascript/Project7 - Persistent ToDo List/script.js
+++ Learning Javascript/Project7 - Persistent ToDo List/script.js
@@ -1,1 +1,16 @@
-let items = []
+let items = [];
+
+const itemsDiv = document.getElementById("items");
+const input = document.getElementById("itemInput");
+
+function loadItems() {
+    insideDiv.innerHTML = null
+}
+
+function renderItems() {}
+
+function saveItems() {}
+
+function addItem() {}
+
+function removeItem() {}

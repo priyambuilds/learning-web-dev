d5ae69642f0773b3eefa86176f53462fc27b14f4
priyambuilds <dpriyam45@gmail.com>
2025-07-10 19:54:49 +0530

    Delete Learning React Js directory

--- Learning React Js/readme.md
+++
@@ -1,7 +0,0 @@
-# Learning React Js
-to get our very basic react app setup, we need to do 2 things.
-```react
-import {createRoot} from "react-dom/client"
-// 1. Create a root
-const root = createRoot()
-// 2. Render some markup to the root
--- Learning React Js/script.jsx
+++
@@ -1,6 +0,0 @@
-// to get our very basic react app setup, we need to do 2 things.
-import { createRoot } from "react-dom/client"
-// 1. Create a root
-const root = createRoot(document.querySelector("#root"))
-// 2. Render some markup to the root
-root.render(<h1>Hello World</h1>)
